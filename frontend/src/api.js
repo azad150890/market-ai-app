00a82bfc6fcde9a31af2f9a8187c062a2476f3d5
azad <azad.learn@learn.com>
2026-05-02 01:03:20 +0530

    frontend calling backend url /api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,6 +4,6 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const getMarketData = (symbol) => {
   return axios.get(
-    `${API_BASE_URL}/api/get_market_data?symbol=${symbol}`
+    `/api/get_market_data?symbol=${symbol}`
   );
 };
