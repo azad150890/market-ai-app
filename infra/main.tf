de05971ff8320cbdc2cb00d9b70912f0cb3444cb
azad <azad.learn@learn.com>
2026-05-01 23:55:56 +0530

    SWA region change and node version

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "marketaisgai19838"
+  name                     = "marketaisgai1038"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier            = "Standard"
--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "rg-tf-storage"
+    storage_account_name  = "tfstateprodmakret"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
