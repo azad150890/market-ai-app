555d714680ca7d827d1b1bb4368eaa51fb8cff6f
azad <azad.learn@learn.com>
2026-05-01 23:47:43 +0530

    SWA region change and node version

--- infra/main.tf
+++ infra/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_linux_function_app" "func" {
 resource "azurerm_static_web_app" "frontend" {
   name                = "market-ai-frontend"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = "Central US"
+  location            = var.location
 
   sku_tier = "Free"
   sku_size = "Free"
